365a3df09f5706f53ab1aa33a839860b4dd2ffe3
YasuriRanasinghe <ar97984@fhss.sjp.ac.lk>
2023-06-14 10:01:36 +0530

    Change price type

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -1,13 +1,13 @@
 export const Prices = [
   {
     _id: 0,
-    name: "$0 to 19",
-    array: [0, 19],
+    name: "Rs. 0 to 1000",
+    array: [0, 1000],
   },
   {
     _id: 1,
-    name: "$20 to 39",
-    array: [20, 39],
+    name: "Rs. 1000 to 2000",
+    array: [1000, 2000],
   },
   {
     _id: 2,
