14caf96773612fef38eed962a2645d7cb9323e15
YasuriRanasinghe <ar97984@fhss.sjp.ac.lk>
2023-06-14 12:59:37 +0530

    Update details

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -43,7 +43,7 @@ const CategoryProduct = () => {
                     <p className="card-text">
                       {p.description.substring(0, 30)}...
                     </p>
-                    <p className="card-text"> Rs. {p.price}</p>
+                    <p className="card-text"> Rs. {p.price}.00</p>
                     <button
                       class="btn btn-primary ms-1"
                       onClick={() => navigate(`/product/${p.slug}`)}
